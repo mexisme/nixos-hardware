e0b086ae7a183143bcbc7755053184523e1635e3
mexisme <wildjim+dev@kiwinet.org>
2021-01-04 00:18:42 +1300

    Update the kernel config for 5.10.2 and 5.10.4

--- microsoft/surface/default.nix
+++ microsoft/surface/default.nix
@@ -6,6 +6,6 @@
     # ./hardware_configuration.nix
   ];
 
-  boot.kernelPackages = surface_kernelPackages.linux_5_10_4;
-  # boot.kernelPatches = surface_kernelPatches.linux_5_10;
+  boot.kernelPackages = surface_kernelPackages.linux_5_10_2;
+  # boot.kernelPatches = surface_kernelPatches.linux_???;
 }
