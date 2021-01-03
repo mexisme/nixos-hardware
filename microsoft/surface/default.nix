caaaf4910f13acc039bec6f17b0393b77f0c823e
mexisme <wildjim+dev@kiwinet.org>
2021-01-03 23:10:23 +1300

    Build 5.10.4 on Surface, by default

--- microsoft/surface/default.nix
+++ microsoft/surface/default.nix
@@ -3,6 +3,9 @@
   imports = [
     ./kernel
     ./firmware
-    ./hardware_configuration.nix
+    # ./hardware_configuration.nix
   ];
+
+  boot.kernelPackages = surface_kernelPackages.linux_5_10_4;
+  # boot.kernelPatches = surface_kernelPatches.linux_5_10;
 }
