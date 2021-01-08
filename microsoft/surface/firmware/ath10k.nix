3417c4897075094b0a0a37823026d5d6c365428a
mexisme <wildjim+dev@kiwinet.org>
2021-01-09 03:31:54 +1300

    Only install QCA6174 firmware, for Surface Go Wifi devices

--- microsoft/surface/firmware/ath10k.nix
+++ microsoft/surface/firmware/ath10k.nix
@@ -6,8 +6,10 @@ stdenv.mkDerivation {
   name = "microsoft-surface-ath10k-firmware";
   src = repos.surface-ath10k-firmware;
   priority = 1;
+
+  # NOTE: It appears that only the QCA6174 directory is needed, to support the Surface Go:
   installPhase = ''
     mkdir -p $out/lib/firmware/ath10k
-    cp -r $src/* $out/lib/firmware/ath10k
+    cp -r $src/QCA6174 $out/lib/firmware/ath10k/
   '';
 }
