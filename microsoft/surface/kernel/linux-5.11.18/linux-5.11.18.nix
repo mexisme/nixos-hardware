caa745f116f29e606e75617ec4c47158d713c8b1
mexisme <wildjim+dev@kiwinet.org>
2021-05-09 22:29:25 +1200

    Add kernel 5.11.18

---
+++ microsoft/surface/kernel/linux-5.11.18/linux-5.11.18.nix
@@ -0,0 +1,18 @@
+{ lib, stdenv, buildPackages, fetchurl, perl, buildLinux, modDirVersionArg ? null, ... } @ args:
+
+with lib;
+
+buildLinux (args // rec {
+  version = "5.11.18";
+
+  # modDirVersion needs to be x.y.z, will automatically add .0 if needed
+  modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
+
+  # branchVersion needs to be x.y
+  extraMeta.branch = versions.majorMinor version;
+
+  src = fetchurl {
+    url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
+    sha256 = "0vyc9wl5gawq17hj3cpp23gl41zm83al5y7wrf4x9kkfssyhn1wb";
+  };
+} // (args.argsOverride or {}))
