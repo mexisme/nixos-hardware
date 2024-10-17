abe7203ccffd857c443035a95adc378ea68c6f5f
mexisme <wildjim+dev@kiwinet.org>
2024-10-17 17:53:50 +1300

    Use SRI "hash" arg for fetch-* functions instead of "sha256"

--- microsoft/surface/common/repos.nix
+++ microsoft/surface/common/repos.nix
@@ -9,17 +9,17 @@
   };
 
   # This is the owner and repo for the pre-patched kernel from the "linux-surface" project:
-  linux-surface-kernel = { rev, sha256 }:
+  linux-surface-kernel = { rev, hash }:
     fetchFromGitHub {
       owner = "linux-surface";
       repo = "kernel";
-      inherit rev sha256;
+      inherit rev hash;
     };
 
   surface-go-ath10k-firmware_backup = fetchFromGitHub {
     owner = "mexisme";
     repo = "linux-surface_ath10k-firmware";
     rev = "74e5409e699383d6ca2bc4da4a8433d16f3850b1";
-    sha256 = "169vgvxpgad9anmchs22fj5qm6ahzjfdnwhd8pc280q705vx6pjk";
+    hash = "sha256-AX48oDmzzEoYQkCmF/201opXuVofwGMUI9qvRt+YVHc=";
   };
 }
